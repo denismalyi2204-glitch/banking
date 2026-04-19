1b88819a5dd033e3281c2afa574371e0f8a83e59
denismalyi2204 <denismalyi2204@gmail.com>
2026-04-19 19:49:25 +0000

    fix: remove _build from git and update .gitignore

--- _build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubumba64/denismalyi2204-glitch/workspace/projects/banking
-# Build directory: /home/ubumba64/denismalyi2204-glitch/workspace/projects/banking/_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(banking_tests "/home/ubumba64/denismalyi2204-glitch/workspace/projects/banking/_build/banking_tests")
-set_tests_properties(banking_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/ubumba64/denismalyi2204-glitch/workspace/projects/banking/CMakeLists.txt;41;add_test;/home/ubumba64/denismalyi2204-glitch/workspace/projects/banking/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
